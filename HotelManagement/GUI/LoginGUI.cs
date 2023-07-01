57387bc9dd790af574db37ee82cc6e7f66487eee
Quach Bao Quan <quachbaoquan123@gmail.com>
2023-07-01 11:10:52 +0700

    Add project files.

---
+++ HotelManagement/GUI/LoginGUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HotelManagement.DAO;
+
+namespace HotelManagement.GUI
+{
+    public partial class LoginGUI : Form
+    {
+        public LoginGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void closebtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "" && txtPassword.Text != "")
+            {
+                if(DBConnector.connect().State == ConnectionState.Open)
+                {
+                    MessageBox.Show("Thành công.");
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không được trống.", "Vui lòng nhập đầy đủ thông tin");
+            }
+        }
+    }
+}
